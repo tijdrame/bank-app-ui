b72a69ed8c22c5406230e56f0f788cab33702abb
Tidiane <tijdrame@gmail.com>
2022-07-22 11:42:21 -0400

    first commit

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from "src/app/model/user.model";
+import { NgForm } from '@angular/forms';
+import { LoginService } from 'src/app/services/login/login.service';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  authStatus: string;
+  model = new User();
+
+  constructor(private loginService: LoginService, private router: Router) {
+
+   }
+
+  ngOnInit(): void {
+
+  }
+
+  validateUser(loginForm: NgForm) {
+    this.loginService.validateLoginDetails(this.model).subscribe(
+      responseData => {
+        window.sessionStorage.setItem('Authorization', responseData.headers.get('Authorization'));
+        this.model = <any> responseData.body;
+        this.model.authStatus = 'AUTH';
+        window.sessionStorage.setItem("userdetails",JSON.stringify(this.model));
+        //add pour gerer les xsrf token (voir dans l'interceptor pour l'envoi ds chq req)
+        //let xsrf = this.getCookie('XSRF-TOKEN');
+        //window.sessionStorage.setItem('XSRF-TOKEN', xsrf);
+        //fin add
+        this.router.navigate(['dashboard']);
+      }, error => {
+        console.log(error);
+      });
+
+  }
+
+  getCookie(name) {
+    let cookie = {};
+    document.cookie.split(';').forEach(function(el) {
+      let [k,v] = el.split('=');
+      cookie[k.trim()] = v;
+    })
+    return cookie[name];
+  }
+
+
+}
